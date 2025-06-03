c3132acfcd19c5944acb6ee32c1b61474d525eb0
Paris <bemorales@scl.edu.gt>
2025-06-03 07:48:01 -0600

    conexiones de files

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { crearSemaforo } from "./components/semaforo/semaforo.js";
+import { header } from "./views/header/header.js";
+
 function carga_dom() {
   let dom = document.querySelector("#root");
   dom.className = "dom";
+  dom.appendChild(header());
   dom.appendChild(crearSemaforo());
   return dom;
 }
